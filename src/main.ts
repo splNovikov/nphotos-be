450ffbfbe374c2a86bc2298ed090cd7512153bf7
Pavel Novikov <spl.novikov@gmail.com>
2019-07-15 11:55:51 -0500

    temporary update for main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5050',
+    origin: 'http://nphotos.ru/',
     methods: 'GET',
   });
-  await app.listen(7777);
+  await app.listen(80);
 }
 
 bootstrap();
