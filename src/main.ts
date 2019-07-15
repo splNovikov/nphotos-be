2e3992ae7515131f1bd6ce8e64a427996945370c
Pavel Novikov <spl.novikov@gmail.com>
2019-07-15 12:39:49 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://nphotos.ru/',
+    origin: 'http://www.nphotos.ru/',
     methods: 'GET',
   });
-  await app.listen(80);
+  await app.listen(process.env.PORT || 7777);
 }
 
 bootstrap();
