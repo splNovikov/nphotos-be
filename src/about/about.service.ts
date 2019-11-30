0ae9dc93589df0e4aa142a0a979eb352e73250e1
Pavel Novikov <spl.novikov@gmail.com>
2019-11-30 16:59:21 +0400

    wrapped getAbout into try catch

--- src/about/about.service.ts
+++ src/about/about.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -11,9 +11,8 @@ export class AboutService {
     @InjectModel('About') private readonly aboutModel: Model<About>) {
   }
 
-  // todo: try catch
-  async getAbout(lang: langs = langs.eng): Promise<AboutDTO[]> {
-    const about = await this.aboutModel.find().exec();
+  public async getAbout(lang: langs = langs.eng): Promise<AboutDTO[]> {
+    const about = await this._getAbout();
 
     return about
       .sort((a, b) => (a.order > b.order) ? 1 : ((b.order > a.order) ? -1 : 0))
@@ -22,5 +21,21 @@ export class AboutService {
         row: lang === langs.rus ? a.row_rus : a.row_eng,
       } as AboutDTO),
     );
+  }
+
+  private async _getAbout(): Promise<About[]> {
+    let about: About[];
+
+    try {
+      about = await this.aboutModel.find().exec();
+    } catch (error) {
+      throw new NotFoundException('Couldn\'t find "about"');
+    }
+
+    if (!about) {
+      throw new NotFoundException('Couldn\'t find "about"');
+    }
+
+    return about;
   }
 }
