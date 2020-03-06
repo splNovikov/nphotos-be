b8f4ee013e6885ec1868a72950a636489a17a711
Pavel Novikov <spl.novikov@gmail.com>
2020-03-06 21:43:40 +0400

    updated dependencies

--- src/main.ts
+++ src/main.ts
@@ -18,24 +18,15 @@ const allowedOrigins = isDev
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-    credentials: true,
-    allowedHeaders:
-      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
-  })
-  // app.enableCors({
-  //   origin: (origin, callback) => {
-  //     if (allowedOrigins.includes(origin) || !origin) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Origin not allowed by CORS'));
-  //     }
-  //   },
-  //   methods: isDev ? 'GET, PUT' : 'GET',
-  // });
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    },
+    methods: isDev ? 'GET, PUT' : 'GET',
+  });
   await app.listen(process.env.PORT || 7777);
 }
 
