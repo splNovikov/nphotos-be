808d3156f325cebd0be14901cdc4e32a93e3b789
Pavel Novikov <spl.novikov@gmail.com>
2020-03-06 21:12:39 +0400

    temporary updated CorsOptions

--- src/main.ts
+++ src/main.ts
@@ -18,9 +18,7 @@ const allowedOrigins = isDev
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      /^(.*)/,
-    ],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 200,
