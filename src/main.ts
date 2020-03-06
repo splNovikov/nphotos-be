b688fb22fb41f4e29e0ff8aa54517494dedb5463
Pavel Novikov <spl.novikov@gmail.com>
2020-03-06 20:58:23 +0400

    temporary updated CorsOptions

--- src/main.ts
+++ src/main.ts
@@ -17,16 +17,16 @@ const allowedOrigins = isDev
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // origin: (origin, callback) => {
-    //   if (allowedOrigins.includes(origin) || !origin) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error('Origin not allowed by CORS'));
-    //   }
-    // },
-    methods: isDev ? 'GET, PUT' : 'GET',
-  });
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     if (allowedOrigins.includes(origin) || !origin) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Origin not allowed by CORS'));
+  //     }
+  //   },
+  //   methods: isDev ? 'GET, PUT' : 'GET',
+  // });
   await app.listen(process.env.PORT || 7777);
 }
 
