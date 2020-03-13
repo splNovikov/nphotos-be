d27c907da7b8ec4bbf21a2fa733e3b56ada710db
Pavel Novikov <spl.novikov@gmail.com>
2020-03-13 12:16:09 +0400

    commented import aws-sdk

--- src/files/s3.config.ts
+++ src/files/s3.config.ts
@@ -1,18 +1,18 @@
-import * as AWS from 'aws-sdk';
-import { PutObjectRequest } from 'aws-sdk/clients/s3';
+// import { config as AWSConfig } from 'aws-sdk';
+import * as S3 from 'aws-sdk/clients/s3';
 
 const {
   AWS_S3_BUCKET_NAME,
   AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY,
 } = process.env;
-AWS.config.update({
-  accessKeyId: AWS_ACCESS_KEY_ID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
-});
-const s3 = new AWS.S3();
+// AWSConfig.update({
+//   accessKeyId: AWS_ACCESS_KEY_ID,
+//   secretAccessKey: AWS_SECRET_ACCESS_KEY,
+// });
+const s3 = new S3();
 
-function s3Params({ key, file }: { key: string; file: any }): PutObjectRequest {
+function s3Params({ key, file }: { key: string; file: any }): S3.PutObjectRequest {
   return {
     Bucket: AWS_S3_BUCKET_NAME,
     ACL: 'public-read',
