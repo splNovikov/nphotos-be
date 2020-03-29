632b6e2553eb510e2e97867fdf8c854d1c1ce84c
Pavel Novikov <spl.novikov@gmail.com>
2020-03-29 07:00:14 +0400

    updated caching for development up to 3 seconds

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,12 @@ import { FilesModule } from './files/files.module';
 import { UserModule } from './user/user.module';
 import { AlbumCategoryModule } from './albumCategory/albumCategory.module';
 import { AlbumImageModule } from './albumImage/albumImage.module';
+import { isDevelopment } from './utils/isDevelopment';
 
 @Module({
   imports: [
-    CacheModule.register({ ttl: 10 * 60 }), // 10 minutes
+    // Cache 3 sec for development, 10 minutes for production
+    CacheModule.register({ ttl: isDevelopment ? 3 : 10 * 60 }),
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
